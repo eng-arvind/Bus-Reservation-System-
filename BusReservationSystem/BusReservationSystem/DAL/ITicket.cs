c49d854c567cb02f1f26f31d988469f7410d890f
3420karthik <3420karthik@gmail.com>
2021-11-23 21:04:20 +0530

    Karthik Added methods to Ticket interface

--- BusReservationSystem/BusReservationSystem/DAL/ITicket.cs
+++ BusReservationSystem/BusReservationSystem/DAL/ITicket.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusReservationSystem.Models;
 
 namespace BusReservationSystem.DAL
 {
-    interface ITicket
+    public interface ITicket
     {
+        public List<Bus> SearchBusByLoc(string arrive, string Dest);
+        public Seat GetSeatBySeatId(int seatid);
+        public bool InsertSeat(Seat seat);
+        public bool BookTicket(Booking book);
+        public bool MakePayment(Payment pay);
+        public bool CancelTicket(int seat, int bookId);
+        public bool UpdateSchedule(int schId, int bookId);
+        public Booking GetTicketDetails(int bookID);
     }
 }

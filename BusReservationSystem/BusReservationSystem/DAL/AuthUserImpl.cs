f22707f204d56f5eec3dde138c41762f9a6dfec8
Nitish6407 <500063395@stu.upes.ac.in>
2021-11-24 11:00:07 +0530

    Nitish Added Authenticate user interface and implimentation method.

--- BusReservationSystem/BusReservationSystem/DAL/AuthUserImpl.cs
+++ BusReservationSystem/BusReservationSystem/DAL/AuthUserImpl.cs
@@ -2,10 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusReservationSystem.Models;
+using BusReservationSystem.Exceptions;
 
 namespace BusReservationSystem.DAL
 {
-    public class AuthUserImpl
+    public class AuthUserImpl: IAuthUser
     {
+        BRSDBContext db = new BRSDBContext();
+
+        public bool AddMoneyWallet(string email, decimal amt)
+        {
+            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
+            user.Wallet += amt;
+            return user.Wallet > 0;
+        }
+
+        public List<Booking> GetLastReservations(string email)
+        {
+            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
+            if (user == null) throw new UserNotFound($"The user with {email} email id is not regestered");
+            return db.Bookings.Where(x => x.UserEmail == user.UserEmail).ToList();
+        }
+
+        public UserReg GetProfile(string email)
+        {
+            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
+            if (user == null) throw new UserNotFound($"The user with {email} email id is not regestered");
+            return user;
+        }
+
+        public UserReg Login(string email, string password)
+        {
+            //Login
+            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
+            if (user == null) throw new UserNotFound($"The user with {email} email id is not regestered");
+            return user;
+
+        }
+
+        public bool ResetPassword(UserReg updated, string email)
+        {
+            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
+            user.Pswd = updated.Pswd;
+            db.Update(user);
+            return db.SaveChanges() == 1;
+        }
+
+        public bool UpdateProfile(UserReg updated, string email)
+        {
+            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
+            user.FirstName = updated.FirstName;
+            user.UserMob = updated.UserMob;
+            user.UsrAddress = updated.UsrAddress;
+            db.Update(user);
+            return db.SaveChanges() == 1;
+        }
     }
 }
--- BusReservationSystem/BusReservationSystem/DAL/IAuthUser.cs
+++ BusReservationSystem/BusReservationSystem/DAL/IAuthUser.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusReservationSystem.Models;
 
 namespace BusReservationSystem.DAL
 {
-    interface IAuthUser
+    public interface IAuthUser
     {
+        public UserReg Login(String email, string password);
+        public bool AddMoneyWallet(String email, decimal amt);
+        public List<Booking> GetLastReservations(string email);
+        public bool UpdateProfile(UserReg updated, string email);
+
+        public UserReg GetProfile(String email);
+        public bool ResetPassword(UserReg updated, string email);
+
     }
 }

2c1290441db2d4372468c4b0fcd7c07491e6541e
3420karthik <3420karthik@gmail.com>
2021-11-24 12:48:55 +0530

    Changing the AuthUser methods

--- BusReservationSystem/BusReservationSystem/DAL/AuthUserImpl.cs
+++ BusReservationSystem/BusReservationSystem/DAL/AuthUserImpl.cs
@@ -11,31 +11,26 @@ namespace BusReservationSystem.DAL
     {
         BRSDBContext db = new BRSDBContext();
 
-        public bool AddMoneyWallet(string email, decimal amt)
+        public bool AddMoneyWallet(UserReg user, decimal amt)
         {
-            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
             user.Wallet += amt;
-            return user.Wallet > 0;
+            return db.SaveChanges()==1;
         }
 
-        public List<Booking> GetLastReservations(string email)
+        public List<Booking> GetLastReservations(UserReg user)
         {
-            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
-            if (user == null) throw new UserNotFound($"The user with {email} email id is not regestered");
             return db.Bookings.Where(x => x.UserEmail == user.UserEmail).ToList();
         }
 
-        public UserReg GetProfile(string email)
+        public UserReg GetProfile(UserReg user)
         {
-            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
-            if (user == null) throw new UserNotFound($"The user with {email} email id is not regestered");
             return user;
         }
 
         public UserReg Login(string email, string password)
         {
             //Login
-            var user = db.UserRegs.Where(x => x.UserEmail == email).FirstOrDefault();
+            var user = db.UserRegs.Where(x => x.UserEmail == email && x.Pswd==password).FirstOrDefault();
             if (user == null) throw new UserNotFound($"The user with {email} email id is not regestered");
             return user;
 
--- BusReservationSystem/BusReservationSystem/DAL/IAuthUser.cs
+++ BusReservationSystem/BusReservationSystem/DAL/IAuthUser.cs
@@ -9,11 +9,11 @@ namespace BusReservationSystem.DAL
     public interface IAuthUser
     {
         public UserReg Login(String email, string password);
-        public bool AddMoneyWallet(String email, decimal amt);
-        public List<Booking> GetLastReservations(string email);
+        public bool AddMoneyWallet(UserReg user, decimal amt);
+        public List<Booking> GetLastReservations(UserReg user);
         public bool UpdateProfile(UserReg updated, string email);
 
-        public UserReg GetProfile(String email);
+        public UserReg GetProfile(UserReg user);
         public bool ResetPassword(UserReg updated, string email);
 
     }

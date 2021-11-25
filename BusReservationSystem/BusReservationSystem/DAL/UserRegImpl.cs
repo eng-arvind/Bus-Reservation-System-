ed05a13892832d90d5de917fe849bf1927daa458
3420karthik <3420karthik@gmail.com>
2021-11-25 09:12:13 +0530

    Added UserAlreadyRegistered exception and changed in UserReg implimentation class

--- BusReservationSystem/BusReservationSystem/DAL/UserRegImpl.cs
+++ BusReservationSystem/BusReservationSystem/DAL/UserRegImpl.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusReservationSystem.Exceptions;
 
 
 namespace BusReservationSystem.DAL
@@ -12,6 +13,8 @@ namespace BusReservationSystem.DAL
         BRSDBContext db = new();
         public bool Register(UserReg newUser)
         {
+            var oldUser = db.UserRegs.Where(x => x.UserEmail == newUser.UserEmail).FirstOrDefault();
+            if (oldUser != null) throw new UserAlreadyRegistered($"User with {newUser.UserEmail} emailId is already registered");
             db.Add(newUser);
             return db.SaveChanges() == 1;
         }

7a82690f1073f1e4ca9411bc7f7efaa86de7481c
3420karthik <3420karthik@gmail.com>
2021-11-25 16:37:58 +0530

    Created UI Project and Added Controllers

---
+++ BusReservationSystem/UIBusReservationSystem/Controllers/AdminUIController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UIBusReservationSystem.Controllers
+{
+    public class AdminUIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

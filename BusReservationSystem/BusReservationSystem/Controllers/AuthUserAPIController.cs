ae51a57083e5a56bbbb7b8321d894fc9c9395afd
Nitish6407 <500063395@stu.upes.ac.in>
2021-11-25 15:28:30 +0530

    Authenticate user api controller is added by nitish

---
+++ BusReservationSystem/BusReservationSystem/Controllers/AuthUserAPIController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusReservationSystem.DAL;
+using BusReservationSystem.Models;
+
+namespace BusReservationSystem.Controllers
+{
+    [Route("api/AuthUserAPI")]
+    [ApiController]
+    public class AuthUserAPIController : ControllerBase
+    {
+        readonly IAuthUser ua;
+        readonly BRSDBContext db;
+
+        public AuthUserAPIController(IAuthUser ua, BRSDBContext db)
+        {
+            this.ua = ua;
+            this.db = db;
+        }
+
+        [HttpPost]
+        [Route("/api/AuthUserAPI/Login/{email}/{password}")]
+        public UserReg Login(String email, string password)
+        {
+            return ua.Login(email, password);
+        }
+        [HttpPost]
+        [Route("/api/AuthUserAPI/AddMoneyWallet/{amt}")]
+
+        public bool AddMoneyWallet(UserReg user, decimal amt)
+        {
+            return ua.AddMoneyWallet(user, amt);
+        }
+        [HttpGet]
+        [Route("/api/AuthUserAPI/GetLastReservations")]
+
+        public List<Booking> GetLastReservations(UserReg user)
+        {
+            return ua.GetLastReservations(user);
+        }
+        [HttpPut]
+        [Route("/api/AuthUserAPI/UpdateProfile/{email}")]
+
+        public bool UpdateProfile(UserReg updated, string email)
+        {
+            return ua.UpdateProfile(updated, email);
+        }
+        [HttpGet]
+        [Route("/api/AuthUserAPI/GetProfile")]
+
+
+        public UserReg GetProfile(UserReg user)
+        {
+            return ua.GetProfile(user);
+        }
+        [HttpPut]
+        [Route("/api/AuthUserAPI/ResetPassword/{email}")]
+
+        public bool ResetPassword(UserReg updated, string email)
+        {
+            return ua.ResetPassword(updated, email);
+        }
+
+    }
+}
+
+
+
+
+

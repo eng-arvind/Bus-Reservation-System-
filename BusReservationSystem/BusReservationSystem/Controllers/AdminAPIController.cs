0d2569abf49ebdf396a93f064ec0ed18d23b2eee
eng-arvind <arvindvishwakarma500@gmail.com>
2021-11-25 10:30:47 +0530

    Controller

---
+++ BusReservationSystem/BusReservationSystem/Controllers/AdminAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusReservationSystem;
+using BusReservationSystem.DAL;
+using BusReservationSystem.Models;
+
+namespace BusReservationSystem.Controllers
+{
+    [Route("api/AdminAPI")]
+    [ApiController]
+    public class AdminAPIController : ControllerBase
+    {
+        IAdmin admin = new AdminImpl();
+        BRSDBContext db = new();
+    }
+}

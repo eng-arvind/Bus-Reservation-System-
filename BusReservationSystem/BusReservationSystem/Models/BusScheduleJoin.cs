5005a367b46101aa8b4df217e107b24ecead8923
3420karthik <3420karthik@gmail.com>
2021-11-26 00:39:26 +0530

    Changed logic of Search bus and implimented homepage and search bus page

---
+++ BusReservationSystem/BusReservationSystem/Models/BusScheduleJoin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusReservationSystem.Models
+{
+    public class BusScheduleJoin
+    {
+        public int BusId { get; set; }
+        public string BusName { get; set; }
+        public int ScheduleId { get; set; }
+        public int? RouteId { get; set; }
+        public DateTime JourneyDate { get; set; }
+        public int? SeatAvail { get; set; }
+        public decimal? Fare { get; set; }
+    }
+}
